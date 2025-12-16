d9ad26171dd85f756d24b6fb7068367759112d53
bmonagan <bernardmonagan@gmail.com>
2025-12-15 20:00:54 -0500

    first time running TS in vscode. need to fix prompt() because it dosent work in the CL

---
+++ main.js
@@ -0,0 +1,39 @@
+var playerScore = 0;
+var compScore = 0;
+function computerChoice(number) {
+    if (number === 0) {
+        return "Rock";
+    }
+    else if (number === 1) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
+function playRound(userchoice, computerChoice) {
+    if (userchoice === computerChoice) {
+        console.log("Its a draw");
+    }
+    else if ((userchoice === 'Rock' && computerChoice === "Scissors") || (userchoice === "Paper" && computerChoice === "Rock") || (userchoice === "Scissors" && computerChoice === "Paper")) {
+        playerScore += 1;
+        console.log("Player wins!");
+    }
+    else {
+        console.log("Computer wins, sad!");
+    }
+}
+var Round = 1;
+while (playerScore < 5 || compScore < 5) {
+    var playerInput = prompt("Enter Rock, Paper, or Scissors");
+    if (playerInput === "Rock" || playerInput === "Paper" || playerInput === "Scissors") {
+        var computerInput = computerChoice(Math.floor(Math.random() * 3));
+        playRound(playerInput, computerInput);
+        console.log("Round: ".concat(Round, " Finished"));
+        Round += 1;
+        console.log("Current Score: Player ".concat(playerScore, " Comp: ").concat(compScore));
+    }
+    else {
+        console.log('Please provide a choice that is either Rock, Paper, or Scissors');
+    }
+}
